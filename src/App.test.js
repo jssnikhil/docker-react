9cb95ec4d45eb15a36d62824ab1b7b74aa4e43c5
Nikhil Kumar <nikhil@Nikhils-MacBook-Pro.local>
2020-05-09 18:12:40 +0800

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/React!!/i);
+  const linkElement = getByText(/lReact!!/i);
   expect(linkElement).toBeInTheDocument();
 });
